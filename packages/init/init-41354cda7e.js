103aeae86182a4ec647a567b5c84cc55ce68ebc2
hbrls <shuaizhexu@gmail.com>
2022-03-13 15:47:48 +0800

    A: head.env.agent

--- packages/init/init-41354cda7e.js
+++
@@ -1,4 +0,0 @@
-/* @head.js/head.js-init 0.1.0 */
-!function(_,e){var n=window.head||{};n.env={},n.env.mode="{{ __mode__ }}",n.env.profile="{{ __profile__ }}",
-n.env.version="{{ __version__ }}",n.env.context="{{ __context__ }}",n.env.rsrc="{{ __rsrc__ }}",
-n.env.api="{{ __api__ }}",window.head=n}(window,document);
--- packages/init/init.js
+++ packages/init/init.js
@@ -5,9 +5,8 @@
   head.env.mode = '{{ __mode__ }}';
   head.env.profile = '{{ __profile__ }}';
   head.env.version = '{{ __version__ }}';
+  head.env.agent = '{{ __agent__ }}';
   head.env.context = '{{ __context__ }}';
-  head.env.rsrc = '{{ __rsrc__ }}';
-  head.env.api = '{{ __api__ }}';
 
   window.head = head;
 }(window, document));

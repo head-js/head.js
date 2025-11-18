d268a00b073e9060db0f97410337331a73162191
hbrls <shuaizhexu@gmail.com>
2025-11-18 21:29:52 +0800

    A: device.vender & publish @head.js/snippet dist/index.cjs 0.5.7

--- packages/guancecom/dist/guancecom-load-from-cdn-e59534.js
+++
@@ -1,8 +0,0 @@
-/* @head.js/snippet-guancecom 3.2.24-8 load-from-cdn */
-!function(){var e,t,n,a;!function(e,t,n,a,r){var o=e[r]=e[r]||{q:[]
-},c=["onReady","init","addAction","addError","setGlobalContext","setUser"];o.factory=function(t){return function(){
-var n=Array.prototype.slice.call(arguments);return o.q.push((function(){var a=e[r];a[t].apply(a,n)})),o}}
-;for(var i=0;i<c.length;i+=1){var s=c[i];o[s]=o.factory(s)}var d=t.createElement(n);d.async=1,d.src="{{ __sdk__ }}"
-;var f=t.getElementsByTagName(n)[0];f.parentNode.insertBefore(d,f)}(window,document,"script",0,"DATAFLUX_RUM"),e=window,
-n=e[t="DATAFLUX_RUM"],a=(new Date).getTime(),n.onReady((function(){var r=(new Date).getTime()
-;(n=e[t]).addAction("PRE_START",{action_message:"cost:"+(r-a)+"ms"})}))}();
---
+++ packages/snippet/guancecom-init-5e1787.js
@@ -0,0 +1,6 @@
+/* @head.js/snippet-guancecom 3.2.24-8 init */
+!function(){var e,o,r;e=window,o=e.DATAFLUX_RUM,r=e.head.agent,o.setGlobalContext({device:{device:r.device.type,
+device_vendor:r.device.vendor,device_model:r.device.model,browser:r.browser.name,browser_version:r.browser.version,
+browser_version_major:r.browser.major,os:r.os.name,os_version:r.os.version,os_version_major:r.os.major}}),
+"UNKNOWN"===r.device.type&&o.addAction("UNKNOW_DEVICE",{action_message:e.navigator.userAgent}),
+window.DATAFLUX_RUM.init("{{ __init__ }}")}();

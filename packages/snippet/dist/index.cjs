f5d1049aaa5a864078901681eb33f97aaeeaa0e5
hbrls <shuaizhexu@gmail.com>
2025-12-08 21:59:38 +0800

    A: guancecom-browser-rum 3.2.24-10 & @head/snippet 0.5.8

--- packages/snippet/dist/index.cjs
+++ packages/snippet/dist/index.cjs
@@ -6,17 +6,17 @@
 var path = require('path');
 var fs = require('fs');
 
-const __FILE_HEAD_AGENT = fs.readFileSync(path.join(__dirname, '../agent-0626b0.js'), 'utf-8').trim();
+const __FILE_HEAD_AGENT = fs.readFileSync(path.join(__dirname, '../agent-100907.js'), 'utf-8').trim();
 
 const HEAD_AGENT = __FILE_HEAD_AGENT;
 
-const __FILE__GUANCECOM_LOAD_FROM_CDN = fs.readFileSync(path.join(__dirname, '../guancecom-load-from-cdn-7b7eae.js'), 'utf-8').trim();
+const __FILE__GUANCECOM_LOAD_FROM_CDN = fs.readFileSync(path.join(__dirname, '../guancecom-load-from-cdn-532d3a.js'), 'utf-8').trim();
 
 function makeGuancecomLoadFromCdn(sdk = 'https://static.guance.com/browser-sdk/v3/dataflux-rum.js') {
   return __FILE__GUANCECOM_LOAD_FROM_CDN.replace('{{ __sdk__ }}', sdk);
 }
 
-const __FILE__GUANCECOM_INIT = fs.readFileSync(path.join(__dirname, '../guancecom-init-5e1787.js'), 'utf-8').trim();
+const __FILE__GUANCECOM_INIT = fs.readFileSync(path.join(__dirname, '../guancecom-init-a45739.js'), 'utf-8').trim();
 
 function makeGuancecomInit(config = {}) {
   return __FILE__GUANCECOM_INIT.replace('"{{ __init__ }}"', JSON.stringify(config));
--- packages/snippet/src/index.js
+++ packages/snippet/src/index.js
@@ -2,17 +2,17 @@ import path from 'path';
 import fs from 'fs';
 
 
-const __FILE_HEAD_AGENT = fs.readFileSync(path.join(__dirname, '../agent-0626b0.js'), 'utf-8').trim();
+const __FILE_HEAD_AGENT = fs.readFileSync(path.join(__dirname, '../agent-100907.js'), 'utf-8').trim();
 export const HEAD_AGENT = __FILE_HEAD_AGENT;
 
 
-const __FILE__GUANCECOM_LOAD_FROM_CDN = fs.readFileSync(path.join(__dirname, '../guancecom-load-from-cdn-7b7eae.js'), 'utf-8').trim();
+const __FILE__GUANCECOM_LOAD_FROM_CDN = fs.readFileSync(path.join(__dirname, '../guancecom-load-from-cdn-532d3a.js'), 'utf-8').trim();
 export function makeGuancecomLoadFromCdn(sdk = 'https://static.guance.com/browser-sdk/v3/dataflux-rum.js') {
   return __FILE__GUANCECOM_LOAD_FROM_CDN.replace('{{ __sdk__ }}', sdk);
 }
 
 
-const __FILE__GUANCECOM_INIT = fs.readFileSync(path.join(__dirname, '../guancecom-init-5e1787.js'), 'utf-8').trim();
+const __FILE__GUANCECOM_INIT = fs.readFileSync(path.join(__dirname, '../guancecom-init-a45739.js'), 'utf-8').trim();
 export function makeGuancecomInit(config = {}) {
   return __FILE__GUANCECOM_INIT.replace('"{{ __init__ }}"', JSON.stringify(config));
 }
